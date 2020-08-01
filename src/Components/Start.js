e94d74b700d5dc70a03c15c5ee1d47f7445b000f
Miguel Rocha <amsrocha@outlook.com>
2020-08-01 20:37:17 +0100

    Fix bugs

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -6,7 +6,7 @@ const Start = ({ handleStart }) => {
       <div className="start-quiz text-center">
         <h1 className="mb-4">Welcome to Hive Quiz</h1>
         <p>In this Quiz, you will learn more about Hive Blockchain.</p>
-        <p>To reach the end of the quiz <b>you can´t fail more than 3 questions</b></p>
+        <p>To reach the end of the game, <b>you can´t fail 3 questions.</b></p>
         <h2 className="mt-5 mb-2">Are you ready?</h2>
         <button className="btn btn-success btn-start" onClick={handleStart} rel="nofollow noopener noreferrer">START</button>
       </div>
